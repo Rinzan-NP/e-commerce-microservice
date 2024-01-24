b61c5abff4a70360483af994e53d91ed9808fa08
Rinzan-NP <nprinzan@gmail.com>
2024-01-24 14:33:57 +0530

    sign up

--- Auth/api/serialize.py
+++ Auth/api/serialize.py
@@ -1,9 +1,26 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer,ValidationError
 from .models import User
-class UserSerializer(ModelSerializer):
+
+
+class UserRegisterSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
-    
-    
-    
+        fields = [
+            "id",
+            "first_name",
+            "last_name",
+            "email",
+            "password",
+            "profile_pic",
+        ]
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop("password", None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+            instance.save()
+            return instance
+        else:
+            raise ValidationError({"password": "password is not valid"})
--- Auth/api/views.py
+++ Auth/api/views.py
@@ -1,5 +1,15 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from .serialize import UserRegisterSerializer
+from rest_framework import status
 
 class SignUpView(APIView):
-    pass
+    def post(self, request):
+        serializer = UserRegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+
+        else:
+            return Response(serializer.errors, status=status.HTTP_406_NOT_ACCEPTABLE)
+        content = {"message": "Accoutn created successfully!"}
+        return Response(content)
